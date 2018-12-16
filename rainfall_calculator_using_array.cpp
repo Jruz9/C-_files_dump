29dc96bca457bf40bdba79c2c2f4fe348c78e4fa
Jcruz9 <josec300@live.com>
2018-12-15 21:53:43 -0500

    This is revision one of the c++ program

---
+++ rainfall_calculator_using_array.cpp
@@ -0,0 +1,49 @@
+//Chapter 7 practice problem 2 Rainfall Statistics
+
+/*
+----------------------------------------------------------------------------
+Write a program that lets the user enter the total rainfall for each of the twelve months
+in a array with doubles.
+
+It should calculate the and display the total rainfall for the year, 
+avg monthly rainfall for the year and  the month with the highest  and lowest amount
+
+with INput VALIDIATION
+
+
+
+
+-----------------------------------------------------------------------------
+*/
+#include  <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+
+void prompt_1
+
+
+
+
+
+int main()
+{
+//this is the list of constants
+int prompt;
+
+prompt=prompt_1;
+	
+}
+
+void prompt_1()
+{
+	cout<<"This is a Progam that let you enter";
+	
+	cout<<"the avg rainfall for each of the twelve months"<<endl;
+	
+	cout<<"It will also tell you the total rainfall, avg rainfall, and highest month of rainfall"<<endl;
+	
+	cout<<"Enter the avg amount of rainfall for each month"<<endl;
+}
+
